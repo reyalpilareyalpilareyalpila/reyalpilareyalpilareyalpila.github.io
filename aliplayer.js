b04bb11194c77452c770860919393aca79eb4a05
reyalpilareyalpilareyalpila <168404748+reyalpilareyalpilareyalpila@users.noreply.github.com>
2024-05-02 00:52:27 +0800

    Update aliplayer.js

--- aliplayer.js
+++ aliplayer.js
@@ -1,1 +1,8 @@
 loadScript("https://")
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?46c6287ce998a2d843c4a07c9f549efa";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
