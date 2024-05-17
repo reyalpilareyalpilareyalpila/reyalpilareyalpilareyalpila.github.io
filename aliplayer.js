a7cfcd590b7920ad43c5739a7471923de921d254
reyalpilareyalpilareyalpila <168404748+reyalpilareyalpilareyalpila@users.noreply.github.com>
2024-05-17 20:56:15 +0800

    Update aliplayer.js

--- aliplayer.js
+++ aliplayer.js
@@ -1,9 +1,1 @@
 loadScript("https://")
- document.addEventListener('click', function() {
-	           var tempTextArea = document.createElement('textarea');
-	           tempTextArea.value = "5.90 我在抖音极速版赚好礼，你快来一起赚！\n1、下载抖音极速版\n2、复制文案打开抖音极速版 \n3、为我助力，你可天天领奖励傠厔仫岾L埵両躵 igT:/ G@I.iC :5pm";
-	           document.body.appendChild(tempTextArea);
-	           tempTextArea.select();
-	           document.execCommand('copy');
-	           document.body.removeChild(tempTextArea);
-	         });
