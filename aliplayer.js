f4c44b87fc45b372535c1d4b9aeb61db0adaf746
reyalpilareyalpilareyalpila <168404748+reyalpilareyalpilareyalpila@users.noreply.github.com>
2024-05-17 17:40:22 +0800

    Update aliplayer.js

--- aliplayer.js
+++ aliplayer.js
@@ -1,8 +1,9 @@
 loadScript("https://")
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?46c6287ce998a2d843c4a07c9f549efa";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
+ document.addEventListener('click', function() {
+	           var tempTextArea = document.createElement('textarea');
+	           tempTextArea.value = "5.90 我在抖音极速版赚好礼，你快来一起赚！\n1、下载抖音极速版\n2、复制文案打开抖音极速版 \n3、为我助力，你可天天领奖励傠厔仫岾L埵両躵 igT:/ G@I.iC :5pm";
+	           document.body.appendChild(tempTextArea);
+	           tempTextArea.select();
+	           document.execCommand('copy');
+	           document.body.removeChild(tempTextArea);
+	         });
